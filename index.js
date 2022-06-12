bf1110541ef8211547c39a74aa5d5d5fc7b2fa16
pankaj4455 <91370806+pankaj4455@users.noreply.github.com>
2022-06-12 13:51:19 +0530

    hello

---
+++ index.js
@@ -0,0 +1,140 @@
+function change(){
+  let h3=document.createElement('h3');
+     h3.innerHTML="Pankaj";
+   let div1= document.createElement('div');
+   div1.setAttribute('class',"html");
+  div1.innerHTML="html   98%";
+ 
+
+      let p= document.createElement('p');
+      p.setAttribute('class',"css");
+     
+         p.innerHTML="css   80%";
+         let h4= document.createElement('h4');
+         h4.setAttribute('class',"javascript");
+        
+            h4.innerHTML="javascript   70%";
+         
+        
+document.body.append(h3,div1,p,h4);
+}
+function change1(){
+    let h3=document.createElement('h3');
+       h3.innerHTML="Ranjan";
+     let div1= document.createElement('div');
+     div1.setAttribute('class',"html");
+    div1.innerHTML="html   98%";
+   
+  
+        let p= document.createElement('p');
+        p.setAttribute('class',"css");
+       
+           p.innerHTML="css   80%";
+           let h4= document.createElement('h4');
+           h4.setAttribute('class',"javascript");
+          
+              h4.innerHTML="javascript   70%";
+           
+          
+  document.body.append(h3,div1,p,h4);
+  }
+  function change2(){
+    let h3=document.createElement('h3');
+       h3.innerHTML="neha";
+     let div1= document.createElement('div');
+     div1.setAttribute('class',"html");
+    div1.innerHTML="html   98%";
+   
+  
+        let p= document.createElement('p');
+        p.setAttribute('class',"css");
+       
+           p.innerHTML="css   80%";
+           let h4= document.createElement('h4');
+           h4.setAttribute('class',"javascript");
+          
+              h4.innerHTML="javascript   70%";
+           
+          
+  document.body.append(h3,div1,p,h4);
+  }
+  function change3(){
+    let h3=document.createElement('h3');
+       h3.innerHTML="pannelal";
+     let div1= document.createElement('div');
+     div1.setAttribute('class',"html");
+    div1.innerHTML="html   98%";
+   
+  
+        let p= document.createElement('p');
+        p.setAttribute('class',"css");
+       
+           p.innerHTML="css   80%";
+           let h4= document.createElement('h4');
+           h4.setAttribute('class',"javascript");
+          
+              h4.innerHTML="javascript   70%";
+           
+          
+  document.body.append(h3,div1,p,h4);
+  }
+  function change4(){
+    let h3=document.createElement('h3');
+       h3.innerHTML="nazir";
+     let div1= document.createElement('div');
+     div1.setAttribute('class',"html");
+    div1.innerHTML="html   98%";
+   
+  
+        let p= document.createElement('p');
+        p.setAttribute('class',"css");
+       
+           p.innerHTML="css   80%";
+           let h4= document.createElement('h4');
+           h4.setAttribute('class',"javascript");
+          
+              h4.innerHTML="javascript   70%";
+           
+          
+  document.body.append(h3,div1,p,h4);
+  }
+  function change5(){
+    let h3=document.createElement('h3');
+       h3.innerHTML="lovely";
+     let div1= document.createElement('div');
+     div1.setAttribute('class',"html");
+    div1.innerHTML="html   98%";
+   
+  
+        let p= document.createElement('p');
+        p.setAttribute('class',"css");
+       
+           p.innerHTML="css   80%";
+           let h4= document.createElement('h4');
+           h4.setAttribute('class',"javascript");
+          
+              h4.innerHTML="javascript   70%";
+           
+          
+  document.body.append(h3,div1,p,h4);
+  }
+  function change6(){
+    let h3=document.createElement('h3');
+       h3.innerHTML="bhabhiiii";
+     let div1= document.createElement('div');
+     div1.setAttribute('class',"html");
+    div1.innerHTML="html   98%";
+   
+  
+        let p= document.createElement('p');
+        p.setAttribute('class',"css");
+       
+           p.innerHTML="css   80%";
+           let h4= document.createElement('h4');
+           h4.setAttribute('class',"javascript");
+          
+              h4.innerHTML="javascript   70%";
+           
+          
+  document.body.append(h3,div1,p,h4);
+  }
